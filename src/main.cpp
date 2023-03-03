886fbd13f060a98dfac4b04b4c9ff05863cbc81c
gunnartiger <Fyzpan-ninhuw-nudto8>
2023-03-03 16:40:08 +0100

    Added time stamps to Data file and related dependency Change calculation method of the corresponding resistance of a state

--- src/main.cpp
+++ src/main.cpp
@@ -1,6 +1,9 @@
 #include <Arduino.h>
 #include <SD.h>
+#include <RTCZero.h>
 #include <ADSWeather.h>
+#include <cmath>
+#include <bitset>
 
 // Activate Serial Output over USB
 #define DEBUGGING
@@ -21,15 +24,15 @@
 // Timing for Windssensor
 #define CALC_INTERVAL_SENSOR 1000
 // Timing for Hill-Climbing
-#define CALC_INTERVAL_RESISTOR 10
+#define CALC_INTERVAL_RESISTOR 100
 
 unsigned long nextCalcSensor;
 unsigned long nextCalcResistance;
 unsigned long timer;
 
 // Variables for comparing and saving voltages
-int old_power;
-int new_power;
+double old_power;
+double new_power;
 
 // Variables for controlling the MOSFETs State to determine which MOSFETS are on,
 // rising cyclye to determine if the loop is in an ascending run or descending.
@@ -38,16 +41,24 @@ int state;
 bool rising_res_cycle;
 const char MOSFETPINS[8] = {MOSFET1, MOSFET2, MOSFET3, MOSFET4, MOSFET5, MOSFET6, MOSFET7, MOSFET8};
 
+// Time object for using the clock
+RTCZero rtc;
+/* Change these values to set the current initial time */
+const byte seconds = 0;
+const byte minutes = 0;
+const byte hours = 0;
+/* Change these values to set the current initial date */
+const byte day = 5;
+const byte month = 3;
+const byte year = 22;
 
-
-int calculate_power(int voltage, int state_i);
+double calculate_power(double voltage, int state_i);
 
 int count_up(int state_i);
 
 int count_down(int state_i);
 
 void switch_transistors(int state_i);
-
 
 
 ADSWeather adsWeather(VANE_PIN, ANEMOMETER_PIN);
@@ -87,6 +98,12 @@ void setup() {
     state = 255;
     rising_res_cycle = true;
 
+    rtc.begin(); // initialize RTC
+    // Set the time
+    rtc.setTime(hours, minutes, seconds);
+    // Set the date
+    rtc.setDate(day, month, year);
+
 #ifdef DEBUGGING
     Serial.begin(9600);
 #endif
@@ -101,24 +118,27 @@ void loop() {
 
     String dataString = "";
 
+
     // Update Sensor Values, call as often as possible.
     adsWeather.update();
 
     //
-    if(timer > nextCalcResistance) {
+
+    if (timer > nextCalcResistance) {
         // Calculate the next time to change the Resistor Cascade
         nextCalcResistance = timer + CALC_INTERVAL_RESISTOR;
         // Calculate the current generated Power, with the current state and the new measured voltage.
-        new_power = calculate_power(analogRead(MEASUREMENT_PIN), state);
+        double volt = analogRead(MEASUREMENT_PIN) * 3.3 / 4095;
+        new_power = calculate_power(volt, state);
 
         // Hill-Climbing Decision
-        if(new_power > old_power) {
-            if(rising_res_cycle){
+        if (new_power > old_power) {
+            if (rising_res_cycle) {
                 state = count_down(state);
-            } else{
+            } else {
                 state = count_up(state);
             }
-        } else if(new_power < old_power) {
+        } else if (new_power < old_power) {
             if (rising_res_cycle) {
                 state = count_up(state);
             } else {
@@ -143,9 +163,11 @@ void loop() {
         windGust = adsWeather.getWindGust();
 
         // Generate one string to be written to SD-Card
+        double voltage = analogRead(MEASUREMENT_PIN) * 3.3 / 4095;
         dataString +=
-                String(windSpeed) + "," + String(windGust) + "," + String(windDirection) + "," + String(new_power) +
-                "," + String(state);
+                String(windSpeed) + "," + String(windGust) + "," + String(windDirection) + "," + new_power +
+                "," + String(state) + "," + voltage + "," + rtc.getMonth() + "/" + rtc.getDay() + "," + rtc.getHours() +
+                ":" + rtc.getMinutes() + ":" + rtc.getSeconds();
 
         // Open the File and Write to it
         File dataFile = SD.open("datalog.txt", FILE_WRITE);
@@ -154,20 +176,32 @@ void loop() {
             dataFile.close();
         }
 
-    #ifdef DEBUGGING
+#ifdef DEBUGGING
         Serial.println(dataString);
-    #endif
+#endif
     }
 
 }
-int calculate_power(int voltage, int state_i){
+
+double calculate_power(double voltage, int state_i) {
+    double resistance = 0;
+    std::bitset<8> bin_x(state_i);
+    // Calculate Resistance by transforming int to binary multply one by res og a mosfet and 0 by the according resistor
+    for (int i = 0; i < 8; i++) {
+        if (bin_x[0] == 1) {
+            resistance += 0.02;
+        } else {
+            resistance += pow(2, i);
+        }
+        bin_x >>= 1;
+    }
     // Calculates the electrical power from a voltage and a resistance, by Using the Formula P = U*I where I = U/R
-    return (voltage * voltage / state_i);
+    return (pow(voltage, 2) / resistance);
 }
 
 int count_up(int state_i) {
     //Increase the State as long as it's less than 255
-    if(state_i == 255){
+    if (state_i == 255) {
         return 255;
     }
     return (state_i + 1);
@@ -175,7 +209,7 @@ int count_up(int state_i) {
 
 int count_down(int state_i) {
     //Decrease the State as long as it's greater than zero
-    if(state_i == 0){
+    if (state_i == 0) {
         return 0;
     }
     return (state_i - 1);
@@ -183,12 +217,12 @@ int count_down(int state_i) {
 
 void switch_transistors(int state_i) {
     //Calculate the binary of the State and switch the Mosfets accordingly (255 all on, 254 all on but the first off...)
-    for (char i : MOSFETPINS) {
-        if (state_i % 2) {
-            digitalWrite(i, HIGH);
+    std::bitset<8> bin_x(state_i);
+    for (int i = 0; i < 8; i++) {
+        if (bin_x[i] == 1) {
+            digitalWrite(MOSFETPINS[i], HIGH);
         } else {
-            digitalWrite(i, LOW);
+            digitalWrite(MOSFETPINS[i], LOW);
         }
-        state_i = state_i / 2;
     }
 }
